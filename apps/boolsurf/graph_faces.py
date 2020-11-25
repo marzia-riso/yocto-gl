29653a2de3f9cfb46fee37b46ff687f194b29911
giacomonazzaro <nazzarogiacomo@gmail.com>
2020-11-25 20:43:44 +0100

    update

--- apps/boolsurf/graph_faces.py
+++ apps/boolsurf/graph_faces.py
@@ -1,4 +1,12 @@
-def get_faces(edges, graph):
+def get_faces(graph):
+	edges = []
+	for node, adj in enumerate(graph):
+		for neighbor in adj:
+			if node < neighbor:
+				edges.append((node, neighbor))
+			else:
+				edges.append((neighbor, node))
+
 	edgeset = set(edges)
 
 	faces = []
@@ -7,10 +15,12 @@ def get_faces(edges, graph):
 
 	while (len(edgeset) > 0):
 		neighbors = graph[path[-1][-1]]
-		next_node = neighbors[(neighbors.index(path[-1][-2]) + 1) % (len(neighbors))]
-		tup = (path[-1][-1], next_node)
+		last_node = path[-1][1]
+		next_node = neighbors[(neighbors.index(path[-1][0]) + 1) % (len(neighbors))]
+		tup = (last_node, next_node)
 		if tup == path[0]:
 			faces.append(path)
+			# clear path
 			path = []
 			for edge in edgeset:
 				path.append(edge)
@@ -20,29 +30,21 @@ def get_faces(edges, graph):
 			path.append(tup)
 			edgeset.discard(tup)
 
-	if (len(path) != 0): faces.append(path)
+	if len(path) != 0: faces.append(path)
 	return iter(faces)
 
 import sys
 import ast
 
 if __name__=="__main__":
-	# graph = []
-	# with open(sys.argv[1]) as file:
-	# 	text = file.read()
-	# 	graph = ast.literal_eval(text)
-
-	edges = []
 	graph = [[1, 5], [0,2], [6,1,7,3], [6, 2, 8, 4], [3, 5], [4, 0], [2, 3], [2, 8], [7, 3]]
-	for node, adj in enumerate(graph):
-		for neighbor in adj:
-			if node < neighbor:
-				edges.append((node, neighbor))
-			else:
-				edges.append((neighbor, node))
 
-	# (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0), (2, 7), (7, 8), (8, 3), (3, 6), (6, 2)]
+	if len(sys.argv) > 1:
+		with open(sys.argv[1]) as file:
+			text = file.read()
+			graph = ast.literal_eval(text)
 
-	faces = get_faces(edges, graph)
+
+	faces = get_faces(graph)
 	for face in faces:
 		print(face)
