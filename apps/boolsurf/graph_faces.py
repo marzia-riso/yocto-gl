2bcadeb428b6d3588733c3328a3aaafd90f36df7
giacomonazzaro <nazzarogiacomo@gmail.com>
2020-11-25 19:49:12 +0100

    simplify python code

--- apps/boolsurf/graph_faces.py
+++ apps/boolsurf/graph_faces.py
@@ -1,18 +1,12 @@
-def get_faces(edges, embedding):
-	edgeset = set()
-	for edge in edges:
-		edge = list(edge)
-		edgeset |= set([(edge[0], edge[1]), (edge[1], edge[0])])
+def get_faces(edges, graph):
+	edgeset = set(edges)
 
 	faces = []
-	path = []
-	for edge in edgeset:
-		path.append(edge)
-		edgeset -= set([edge])
-		break
+	path = [edges[0]]
+	edgeset.discard(edges[0]);
 
 	while (len(edgeset) > 0):
-		neighbors = embedding[path[-1][-1]]
+		neighbors = graph[path[-1][-1]]
 		next_node = neighbors[(neighbors.index(path[-1][-2]) + 1) % (len(neighbors))]
 		tup = (path[-1][-1], next_node)
 		if tup == path[0]:
@@ -20,20 +14,35 @@ def get_faces(edges, embedding):
 			path = []
 			for edge in edgeset:
 				path.append(edge)
-				edgeset -= set([edge])
+				edgeset.discard(edge)
 				break
 		else:
 			path.append(tup)
-			edgeset -= set([tup])
+			edgeset.discard(tup)
 
 	if (len(path) != 0): faces.append(path)
 	return iter(faces)
 
+import sys
+import ast
 
 if __name__=="__main__":
-	edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0), (2, 7), (7, 8), (8, 3), (3, 6), (6, 2)]
-	embedding = {0: [1, 5], 1: [0,2], 2: [6,1,7,3], 3: [6, 2, 8, 4], 4: [3, 5], 5:[4, 0], 6: [2, 3], 7: [2, 8], 8: [7, 3]}
+	# graph = []
+	# with open(sys.argv[1]) as file:
+	# 	text = file.read()
+	# 	graph = ast.literal_eval(text)
 
-	faces = get_faces(edges, embedding)
+	edges = []
+	graph = [[1, 5], [0,2], [6,1,7,3], [6, 2, 8, 4], [3, 5], [4, 0], [2, 3], [2, 8], [7, 3]]
+	for node, adj in enumerate(graph):
+		for neighbor in adj:
+			if node < neighbor:
+				edges.append((node, neighbor))
+			else:
+				edges.append((neighbor, node))
+
+	# (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0), (2, 7), (7, 8), (8, 3), (3, 6), (6, 2)]
+
+	faces = get_faces(edges, graph)
 	for face in faces:
 		print(face)
