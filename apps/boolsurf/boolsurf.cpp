39fc71f7b5cec187a24f1d1f55039a1aaadf4b38
Giacomo Nazzaro <nazzarogiacomo@gmail.com>
2020-12-17 17:55:53 +0100

    offset patch

--- apps/boolsurf/boolsurf.cpp
+++ apps/boolsurf/boolsurf.cpp
@@ -587,13 +587,24 @@ void set_patch_shape(
     shade_shape* shape, const bool_mesh& mesh, const vector<int>& faces) {
   auto positions = vector<vec3f>(faces.size() * 3);
   for (int i = 0; i < faces.size(); i++) {
-    positions[3 * i + 0] = mesh.positions[mesh.triangles[faces[i]].x];
-    positions[3 * i + 1] = mesh.positions[mesh.triangles[faces[i]].y];
-    positions[3 * i + 2] = mesh.positions[mesh.triangles[faces[i]].z];
+    auto [a, b, c]       = mesh.triangles[faces[i]];
+    positions[3 * i + 0] = mesh.positions[a] + 0.01 * mesh.normals[a];
+    positions[3 * i + 1] = mesh.positions[b] + 0.01 * mesh.normals[b];
+    positions[3 * i + 2] = mesh.positions[c] + 0.01 * mesh.normals[c];
   }
   set_positions(shape, positions);
   set_instances(shape, {});
   shape->shape->elements = ogl_element_type::triangles;
+}
+
+auto add_patch_shape(
+    app_state* app, const vector<int>& faces, const vec3f& color) {
+  auto patch_shape    = add_shape(app->glscene, {}, {}, {}, {}, {}, {}, {}, {});
+  auto patch_material = add_material(
+      app->glscene, {0, 0, 0}, color, 1, 0, 0.4);  // @Leak
+  add_instance(app->glscene, identity3x4f, patch_shape, patch_material);
+  set_patch_shape(patch_shape, app->mesh, faces);
+  return patch_shape;
 }
 
 void do_the_thing(app_state* app) {
@@ -737,27 +748,29 @@ void do_the_thing(app_state* app) {
   }
 
   app->mesh.normals = compute_normals(app->mesh.triangles, app->mesh.positions);
-  set_positions(app->mesh_shape, {});  // app->mesh.positions);
-  set_triangles(app->mesh_shape, {});  // app->mesh.triangles);
+  set_positions(app->mesh_shape, app->mesh.positions);
+  set_triangles(app->mesh_shape, app->mesh.triangles);
 
   init_edges_and_vertices_shapes_and_points(app);
 
-  {
-    auto patch_shape = add_shape(
-        app->glscene, {}, {}, {}, {}, {}, {}, {}, {});
-    auto patch_material = add_material(
-        app->glscene, {0, 0, 0}, {1, 0, 0}, 1, 0, 0.4);
-    add_instance(app->glscene, identity3x4f, patch_shape, patch_material);
-    set_patch_shape(patch_shape, app->mesh, app->polygons[0].inner_faces);
+  for (auto& polygon : app->polygons) {
+    add_patch_shape(app, polygon.inner_faces, {1, 0, 0});
+    add_patch_shape(app, polygon.outer_faces, {0, 0, 1});
   }
-  {
-    auto patch_shape = add_shape(
-        app->glscene, {}, {}, {}, {}, {}, {}, {}, {});
-    auto patch_material = add_material(
-        app->glscene, {0, 0, 0}, {0, 0, 1}, 1, 0, 0.4);
-    add_instance(app->glscene, identity3x4f, patch_shape, patch_material);
-    set_patch_shape(patch_shape, app->mesh, app->polygons[0].outer_faces);
-  }
+  // {
+  //   auto patch_shape = add_shape(app->glscene, {}, {}, {}, {}, {}, {}, {},
+  //   {}); auto patch_material = add_material(
+  //       app->glscene, {0, 0, 0}, {1, 0, 0}, 1, 0, 0.4);
+  //   add_instance(app->glscene, identity3x4f, patch_shape, patch_material);
+  //   set_patch_shape(patch_shape, app->mesh, app->polygons[0].inner_faces);
+  // }
+  // {
+  //   auto patch_shape = add_shape(app->glscene, {}, {}, {}, {}, {}, {}, {},
+  //   {}); auto patch_material = add_material(
+  //       app->glscene, {0, 0, 0}, {0, 0, 1}, 1, 0, 0.4);
+  //   add_instance(app->glscene, identity3x4f, patch_shape, patch_material);
+  //   set_patch_shape(patch_shape, app->mesh, app->polygons[0].outer_faces);
+  // }
 
   //   for (auto i = 0; i < value.size() - 1; i++) {
   //     auto& segmentAB = value[i];
